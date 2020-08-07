24f1c7977c7b3d3078c7af369f5abb436e2b1030
Delf <pk.delf@gmail.com>
2020-08-07 12:51:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import threading
+
+import pika
+import serial
+import random
+from retry import retry
+
+
+def callback(ch, method, properties, body):
+    threadLock.acquire()
+    print(body, end="\n")
+    try:
+        ser = serial.Serial('/dev/ttyUSB0', 115200)
+        ser.write(body)
+        ser.close()
+    except IOError as error:
+        print("arduino serial not connected")
+    finally:
+        threadLock.release()
+
+
+@retry(pika.exceptions.AMQPConnectionError, delay=5, jitter=(1, 3))
+def consume():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='wireguard.sokol.delfserver.ru',
+                                  credentials=pika.PlainCredentials(username="hardware", password="Ohl@iu8ath#aeLai")))
+    channel = connection.channel()
+    result = channel.queue_declare('translator', exclusive=True)
+    queue_name = result.method.queue
+    channel.queue_bind(exchange='backend.exchange', queue=queue_name, routing_key="hardware.device.control.request")
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+    try:
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        channel.stop_consuming()
+        connection.close()
+
+
+threadLock = threading.Lock()
+
+if __name__ == '__main__':
+    consume()
