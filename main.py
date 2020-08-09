4a7c8ae155826b35f9cdccaf84c0852d3d4615d7
Delf <pk.delf@gmail.com>
2020-08-09 15:49:05 +0300

    add back channel to logs

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ def callback(ch, method, properties, body):
         print("arduino serial not connected")
     finally:
         threadLock.release()
+    send_to_global_log("arduino serial not connected")
+
+
+def send_to_global_log(message):
+    global channel
+    channel.basic_publish(exchange='', routing_key='logs_queue', body=message)
 
 
 @retry(pika.exceptions.AMQPConnectionError, delay=5, jitter=(1, 3))
@@ -25,8 +31,9 @@ def consume():
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='wireguard.sokol.delfserver.ru',
                                   credentials=pika.PlainCredentials(username="hardware", password="Ohl@iu8ath#aeLai")))
+    global channel
     channel = connection.channel()
-    result = channel.queue_declare('translator', exclusive=True)
+    result = channel.queue_declare('translator', exclusive=True, durable=True)
     queue_name = result.method.queue
     channel.queue_bind(exchange='backend.exchange', queue=queue_name, routing_key="hardware.device.control.request")
     channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
@@ -38,6 +45,7 @@ def consume():
 
 
 threadLock = threading.Lock()
+channel = None
 
 if __name__ == '__main__':
     consume()
